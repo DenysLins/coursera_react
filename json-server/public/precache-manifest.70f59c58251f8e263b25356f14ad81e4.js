8a246351e285da0edad81c1b3789cb4852dcbf38
Denys Lins <denyslins@gmail.com>
2018-11-24 22:39:01 -0300

    Add build to json-server

---
+++ json-server/public/precache-manifest.70f59c58251f8e263b25356f14ad81e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "def2847addcbca1630a5",
+    "url": "/static/css/main.95201f13.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "10762ad4ff601ba73f4b",
+    "url": "/static/js/1.10762ad4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "def2847addcbca1630a5",
+    "url": "/static/js/main.def2847a.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "10762ad4ff601ba73f4b",
+    "url": "/static/css/1.5f514e4c.chunk.css"
+  },
+  {
+    "revision": "745386ccf0d1c1890a9305d8c4ea756e",
+    "url": "/index.html"
+  }
+];
